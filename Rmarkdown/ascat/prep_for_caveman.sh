d19160443803b52ccffa0689578350beb887492b
davidroberson <david.roberson@gmail.com>
2016-08-29 18:40:01 +0000

    fixed prep_for_caveman.sh reference to folder for the csv file.

--- Rmarkdown/ascat/prep_for_caveman.sh
+++ Rmarkdown/ascat/prep_for_caveman.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-perl -ne '@F=(split q{,}, $_)[1,2,3,4]; $F[1]-1; print join(\"\t\",@F).\"\n\";' < copynumber.caveman.csv > ./normal_cn.bed 
-perl -ne '@F=(split q{,}, $_)[1,2,3,6]; $F[1]-1; print join(\"\t\",@F).\"\n\";' < copynumber.caveman.csv > ./tumour_cn.bed
+perl -ne '@F=(split q{,}, $_)[1,2,3,4]; $F[1]-1; print join(\"\t\",@F).\"\n\";' < ascat_output/copynumber.caveman.csv > ./normal_cn.bed 
+perl -ne '@F=(split q{,}, $_)[1,2,3,6]; $F[1]-1; print join(\"\t\",@F).\"\n\";' < ascat_output/copynumber.caveman.csv > ./tumour_cn.bed
